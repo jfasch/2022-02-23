2f246e2ab0bba184e73f14f2b3e216fd860c6f87
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-02-24 10:49:47 +0100

    commandline (2)

--- livehacking/commandline.py
+++ livehacking/commandline.py
@@ -12,3 +12,10 @@ print('sys.argv[2]: ', sys.argv[2])
 print('sys.argv[3]: ', sys.argv[3])
 print('sys.argv[4]: ', sys.argv[4])
 print('sys.argv[5]: ', sys.argv[5])
+
+
+my_number_as_str = sys.argv[5]
+print(my_number_as_str, type(my_number_as_str))
+
+my_number_as_int = int(my_number_as_str)
+print(my_number_as_int, type(my_number_as_int))
