dc4c6c53b4fa9b69f6fcf54975c6842c3234c88e
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-03-17 14:04:38 +0100

    simu userdb

--- userdb/simu_userdb.py
+++ userdb/simu_userdb.py
@@ -1,1 +1,8 @@
-# fix this!
+class SimuUserDB:
+    def __init__(self):
+        self.db = {}
+    def insert(self, user):
+        self.db[user.id] = user
+    def search(self, uid):
+        found = self.db[uid]
+        return found
