9f6c6cd0e9a3bc2747e68a585f78c7d1daa90ec9
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-03-17 11:24:49 +0100

    reuirement #34561: simulated user db

---
+++ userdb/tests/test_simu_db.py
@@ -0,0 +1,22 @@
+import simu_userdb
+from user import User
+
+
+def test_insert_all_fine():
+    db = simu_userdb.SimuUserDB()
+    u1 = User(1, 'Joerg', 'Faschingbauer', '19.6.1966')
+    u2 = User(2, 'Caro', 'Faschingbauer', '25.4.1997')
+    db.insert(u1)
+    db.insert(u2)
+    
+    u1_found = db.search(1)
+    assert u1_found.id == u1.id
+    assert u1_found.firstname == u1.firstname
+    assert u1_found.lastname == u1.lastname
+    assert u1_found.birth == u1.birth
+
+    u2_found = db.search(2)
+    assert u2_found.id == u2.id
+    assert u2_found.firstname == u2.firstname
+    assert u2_found.lastname == u2.lastname
+    assert u2_found.birth == u2.birth
