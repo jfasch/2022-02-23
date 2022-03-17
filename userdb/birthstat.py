d46ae55d603435be8f6ec834cfc484d27e4463c2
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-03-17 15:42:21 +0100

    requirement #11765214 implemented

--- userdb/birthstat.py
+++ userdb/birthstat.py
@@ -1,1 +1,13 @@
-# fix this!!
+from collections import defaultdict
+
+
+class BirthStat:
+    def __init__(self):
+        self.counters = defaultdict(int)
+
+    def count(self, users):
+        for u in users:
+            self.counters[u.birth] += 1
+
+    def num_occurences(self, birth):
+        return self.counters[birth]
